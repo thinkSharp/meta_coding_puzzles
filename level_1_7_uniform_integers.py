ec2240512caf4c79c97321f0dc5bcfe96c248e4f
Vishal Bajoria <vkbajoria@gmail.com>
2024-07-30 19:05:39 -0400

    fixing uniform integer implemention

--- level_1_7_uniform_integers.py
+++ level_1_7_uniform_integers.py
@@ -7,29 +7,22 @@ Example: 1 9 => 1,2,3,4,5,6,7,8,9 => 9
 Example: 999999999999 99999999999 => 1 because 99999999999 is inclusive and uniform
 
 Pseudo Code:
-calculate start:
-   divide by 10 if co
-      last uniform number is 10 - 1 = 9
-      9 - start = x
-   divide by 10 * 10 :
-     100 - 1 = 99 - 11 = 88 - 11 = 77 - 11 = 66 exit 
-     
-   divide by 100 * 10 :
-      1000 - 1 = 999 - 111 = 888 -111 = 777 - 111 = 666 - 111 = 
-      
-      
-   middle 100 till 100000 * 10
-      + 10
-single digit count them all
-double digit 11,22,33, 44, 55, so on
-same with third and so on
-
-take first 
+    loop through the len of B:
+        loop through 1 to 9:
+            build uniform digit
+            compare with A and B
+                Count++
 """
 def getUniformIntegerCountInInterval(A: int, B: int) -> int:
   # Write your code here
+    count = 0
+    for digits in range(1, len(str(B))+1):
+       for digit in range(1,10):
+          uniform_num = int(str(digit) * digits)
+          if A <= uniform_num <= B:
+             count +=1
+    return count
 
-  return count
 
 
 class TestSolution(unittest.TestCase):
